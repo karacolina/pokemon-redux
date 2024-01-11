7ebc86966cc91c2ba2f74e076c868f7ce02b40f3
Carolina Correa <carolinacorrea@MacBook-Pro-de-Carolina.local>
2024-01-11 18:07:59 -0300

    feat: Added tailwind CSS to manage styles

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Roboto Condensed", "sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+      },
+    },
+  },
+};
